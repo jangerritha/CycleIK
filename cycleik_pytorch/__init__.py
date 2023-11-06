2e6667cb308779e2aa2c0d511c9c7fd400bb7bbe
Jan-Gerrit Habekost <jan-gerrit.habekost@uni-hamburg.de>
2023-11-06 18:32:00 +0100

    fix package

--- cycleik_pytorch/__init__.py
+++ cycleik_pytorch/__init__.py
@@ -1,6 +1,6 @@
 from .utils import weights_init, load_config, get_kinematic_params, slice_fk_pose, normalize_pose, renormalize_pose, renormalize_joint_state
 from .datasets import IKDataset
-from .models import AutoEncoder, GenericDiscriminator, GenericGenerator, GenericNoisyGenerator, FineTuneModel
+from .models import GenericGenerator, GenericNoisyGenerator
 from .optim import DecayLR
 from .utils import ReplayBuffer
 from .predictor import CycleIK
@@ -8,12 +8,9 @@ from .predictor import CycleIK
 
 __all__ = [
     "IKDataset",
-    "FineTuneModel",
-    "AutoEncoder",
     "DecayLR",
     "ReplayBuffer",
     "weights_init",
-    "GenericDiscriminator",
     "GenericGenerator",
     "GenericNoisyGenerator",
     "load_config",
